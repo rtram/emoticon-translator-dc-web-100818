add0b191c199a32d91d16bda35f43fd6fdaa7091
Robin Tram <robin.tram@gmail.com>
2018-10-05 18:39:19 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,33 @@
 # require modules here
+require "pry"
 
-def load_library
-  # code goes here
+def load_library (filepath)
+  require "yaml"
+  filepath_hash = YAML.load_file(filepath)
+  
+  dictionary = {}
+  get_meaning = {}
+  get_jap_emote = {}
+  
+  #create get_meaning hash where jap_emote => eng_meaning
+  filepath_hash.each do |meaning, emotes|
+    get_meaning[emotes[1]] = meaning
+  end 
+  #create get_jap_emote hash where eng_emote => jap_emote
+  filepath_hash.each do |meaning, emotes|
+    get_jap_emote[emotes[0]] = emotes[1]
+  end 
+  dictionary["get_meaning"] = get_meaning
+  dictionary["get_emoticon"] = get_jap_emote
+
+  return dictionary
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon (filepath, emoticon)
+  load_library(filepath)
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning (filepath, emoticon)
+  load_library(filepath)
+  
 end
